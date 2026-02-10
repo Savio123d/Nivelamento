31af83e580eb3bb36f681d8a1b2228ecbad71683
Savio123d <savio13foxy@gmail.com>
2026-02-09 22:26:36 -0300

    fazendo

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
         Scanner leitor = new Scanner(System.in);
+
         System.out.printf("1-Moto \n2-Caminhoes. ");
+        int s=leitor.nextInt();
+        if(s==1){
+            Veiculo moto = new Moto();
+
+        }else if(s==2){
+            Veiculo moto = new Caminhoes();
+
+        }
 
         System.out.printf("Modelo: ");
 
